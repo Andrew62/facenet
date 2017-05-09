a13225b0502e44a0062d9283f56c71cc976bc3ec
Andrew62 <awoizesko@gmail.com>
2017-05-08 20:47:33 -0400

    added a func to make sure directories exist

--- helper.py
+++ helper.py
@@ -1,4 +1,11 @@
+import os
 import time
+
 
 def get_current_timestamp():
     return time.strftime("%Y-%m-%d-%H%M", time.localtime())
+
+
+def check_dir(fp):
+    if not os.path.exists(fp):
+        os.makedirs(fp, exist_ok=True)
