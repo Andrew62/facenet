9b3f582b52f42f5d5d1a22f4bf3073f1bc0a9182
Andrew62 <awoizesko@gmail.com>
2017-05-14 19:09:31 -0400

    using norm instead of l2_normalize

--- loss.py
+++ loss.py
@@ -7,8 +7,8 @@ def loss_func(anchors, positives, negatives, alpha=0.2):
     Loss function pulled form FaceNet paper. Loss function is
         sum([||f(anchor) - f(positive))||**2] - [||f(anchor) - f(negative)||**2] + alpha)
     """
-    positive_dist = tf.pow(tf.nn.l2_normalize(anchors - positives, 0), 2)
-    negative_dist = tf.pow(tf.nn.l2_normalize(anchors - negatives, 0), 2)
+    positive_dist = tf.pow(tf.norm(anchors - positives, axis=1, ord=2), 2)
+    negative_dist = tf.pow(tf.norm(anchors - negatives, axis=1, ord=2), 2)
     return tf.reduce_sum(positive_dist - negative_dist + alpha)
 
 
