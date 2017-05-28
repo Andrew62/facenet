6587f03b77f491ea5ac5cdf8322d1875e3102592
Andrew62 <awoizesko@gmail.com>
2017-05-28 10:01:06 -0400

    changed to command line program

--- collect_images.py
+++ collect_images.py
@@ -1,32 +1,37 @@
 import os
 import json
+import argparse
 
-# input directory where each subfolder is the unique name of the
-# person. Will look for jpegs, gifs, and pngs
-base_dir = "fixtures/faces"
+def main():
 
-# output csv for the training input producer
-out_json = "fixtures/faces.json"
+    parser = argparse.ArgumentParser(description="Program to collect image paths and output a json file")
+    parser.add_argument("-i", "--in_dir", help="top directory to walk", required=True,
+                        type=str)
+    parser.add_argument("-o", "--out_file", help="output json file",
+                        default="images.json", dtype=str)
+    parser.add_argument("-e", "--exts", help="file extensions to look for",
+                        default=[".jpeg", ".gif", ".jpg", ".png"], nargs="+")
+    args = parser.parse_args()
 
-# store all images by class
-images = {}
+    # store all images by class
+    images = {}
 
-# these are the images exts that tf.decode_image can handle
-exts = [".jpeg", ".gif", ".jpg", ".png"]
+    # collect all images files first so we can sort
+    # the class names alphabetically then get their index
+    for d, _, files in os.walk(args.in_dir):
+        for f in files:
+            if any(map(lambda x: f.endswith(x), args.exts)):
+                class_name = os.path.basename(d)
+                if class_name not in images.keys():
+                    images[class_name] = []
+                images[class_name].append(os.path.join(os.path.abspath(d), f))
 
-# collect all images files first so we can sort
-# the class names alphabetically then get their index
-for d, _, files in os.walk(base_dir):
-    for f in files:
-        if any(map(lambda x: f.endswith(x), exts)):
-            class_name = os.path.basename(d)
-            if class_name not in images.keys():
-                images[class_name] = []
-            images[class_name].append(os.path.join(os.path.abspath(d), f))
+    # write out csv of all combinations of positives. Later we'll
+    # sample negatives from within a minibatch
+    with open(args.out_file, 'w', newline='') as target:
+        json.dump(images, target, indent=2)
 
-# write out csv of all combinations of positives. Later we'll
-# sample negatives from within a minibatch
-with open(out_json, 'w', newline='') as target:
-    json.dump(images, target, indent=2)
+    print("done")
 
-print("done")
+if __name__ == "__main__":
+    main()
